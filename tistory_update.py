074bd0011d8f3fb6261aba723eea0f3b2d0bf145
ichhjkim <52682733+ichhjkim@users.noreply.github.com>
2023-09-13 13:18:10 +0900

    Update tistory_update.py

--- tistory_update.py
+++ tistory_update.py
@@ -20,11 +20,10 @@ markdown_text = """
 
 """ 
  
-dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
- 
 for content in feed['entries'][:3]:
     markdown_text += f"<a href =\"{content['link']}\"> {content['title']} </a> <br>"
     print(content['link'], content['title'])
+    print(content)
  
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
